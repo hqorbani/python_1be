e510ff0f65329cc4f36d62196ae833daba858e92
Hamzeh <h.qorbani7@gmail.com>
2023-03-06 09:13:21 +0330

    session_4: talking about for range

---
+++ session_4/index.py
@@ -0,0 +1,12 @@
+b = 5
+a = 6
+print( a == b)
+print( a != b)
+print( a < b)
+print( a <= b)
+print( a > b)
+print( a >= b)
+
+if a >= b:
+    print("dfgd gdfg")
+    
---
+++ session_4/loop_for.py
@@ -0,0 +1,7 @@
+# print(range(6))
+# print(range(3,6))
+
+for i in range(1,20):
+    i = i + 1
+    print(i)
+    
